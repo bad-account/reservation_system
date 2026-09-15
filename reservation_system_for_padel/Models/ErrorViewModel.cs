60d71eef140363e9c31a4e7b7108d4b272622b73
Eva Trda <trd0006@vsb.cz>
2026-09-15 19:45:04 +0200

    New project

---
+++ reservation_system_for_padel/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reservation_system_for_padel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
